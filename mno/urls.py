882f2a55a55c3f430d10fb809bb271cf79e825d6
Ghsa-star <gaith.alelyani@gmail.com>
2025-10-03 22:52:44 +0300

    جمعت

--- mno/urls.py
+++ mno/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,8 @@ urlpatterns = [
     path('consultations/', include('consultations.urls')),  # الاستشارات والمتابعات
     path('payments/', include('payments_plans.urls')),      # الدفع والخطط
 ]
+
+# إضافة مسارات الملفات الثابتة والميديا أثناء التطوير
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
