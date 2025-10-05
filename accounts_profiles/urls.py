46836c7e1df6d7b304d482e5445b1790f9cbba28
Ghsa-star <gaith.alelyani@gmail.com>
2025-10-05 19:44:32 +0300

    اضفت رزين بعد الصيانة

--- accounts_profiles/urls.py
+++ accounts_profiles/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # روابط تطبيق الحسابات والملفات
+    path("register/", views.register_view, name="register"),  # إنشاء حساب جديد
+    path("login/", views.login_view, name="login"),            # تسجيل الدخول
+    path("logout/", views.logout_view, name="logout"),          # تسجيل الخروج
 ]
