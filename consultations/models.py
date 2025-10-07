91fe965e2766807f0f3edd701685c45185759d31
Ghsa-star <gaith.alelyani@gmail.com>
2025-10-07 21:14:35 +0300

    ما قبل انشاء قاعدة البيانات

--- consultations/models.py
+++ consultations/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from cloudinary.models import CloudinaryField  # ✅ استيراد الحقل من Cloudinary
+from cloudinary.models import CloudinaryField
 
 class Consultation(models.Model):
     title = models.CharField("عنوان الاستشارة", max_length=200)
-    scheduled_date = models.DateTimeField("تاريخ ووقت الموعد")
-    image = CloudinaryField("صورة الاستشارة", folder="consultations_images", blank=True, null=True)  # ✅ ربط فعلي بـ Cloudinary
+    image = CloudinaryField("صورة الاستشارة", folder="consultations_images", blank=True, null=True)
 
     class Meta:
         verbose_name = "استشارة"
