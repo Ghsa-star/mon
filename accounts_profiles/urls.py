64eb0dcef43ac9362123f7a7609adddc5d6e9ef9
Ghsa-star <gaith.alelyani@gmail.com>
2025-10-08 20:49:27 +0300

    إصلاح تكرار المسارات في ملف urls.py

--- accounts_profiles/urls.py
+++ accounts_profiles/urls.py
@@ -2,12 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-=======
     path("register/", views.register_view, name="register"),  # إنشاء حساب جديد
-    path("login/", views.login_view, name="login"),            # تسجيل الدخول
-    path("logout/", views.logout_view, name="logout"),          # تسجيل الخروج
->>>>>>> stable
+    path("login/", views.login_view, name="login"),           # تسجيل الدخول
+    path("logout/", views.logout_view, name="logout"),        # تسجيل الخروج
 ]
