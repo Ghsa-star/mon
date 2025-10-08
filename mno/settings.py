e9edd69f36d468bd64545bcb45e02d37584d4842
Ghsa-star <gaith.alelyani@gmail.com>
2025-10-08 20:16:35 +0300

    Fix ALLOWED_HOSTS for Render

--- mno/settings.py
+++ mno/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ------------------------------------------------------------
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ['mon-z64j.onrender.com', '127.0.0.1', 'localhost']
 
 
 # ------------------------------------------------------------
